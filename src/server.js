3be0e4e64b6b2edc53b03fda5131584188337039
Marianne Anderson <mganderson13@gmail.com>
2024-05-29 14:15:18 -0500

    params notes and upvote endpoint

--- src/server.js
+++ src/server.js
@@ -1,10 +1,36 @@
 import express from "express";
 
-const app = express();
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+}, {
+    name: 'learn-node',
+    upvotes: 0,
+}, {
+    name: 'mongodb',
+    upvotes: 0,
+}]
 
-app.get('/hello', (req, res) => {
-    res.send("Hello!");
-});
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:articleId/upvote', (req, res) => {
+    const { articleId } = req.params;
+    const article = articlesInfo.find(a => a.name === articleId);
+    if (article) {
+    article.upvotes +=1;
+    res.send(`The ${articleId} article now has ${article.upvotes} upvotes`);
+    }else {
+        res.send(`That article does not exist`);
+    }
+})
+
+// Params in requests
+// app.get('/hello/:name/goodbye/:secondName', (req, res) => {
+//     console.log(req.params); is going to give- { name: 'Marianne', secondName: 'Emma'} depending on value of params in the url
+//     const { name } = req.params; this will pull name param from url
+//     res.send(`Hello ${name}!!`); can than use value of name param in response
+// })
 
 app.listen(8000, () => {
     console.log("Server is listening on port 8000");
