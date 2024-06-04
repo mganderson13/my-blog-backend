4952fd41964e5676efb11df82425e4b4b9008ad6
Marianne Anderson <mganderson13@gmail.com>
2024-06-04 09:21:30 -0500

    changed :articleId to :name in endpoints

--- src/server.js
+++ src/server.js
@@ -1,24 +1,38 @@
 import express from "express";
+import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(express.json());
 
-app.put('/api/articles/:articleId/upvote', (req, res) => {
-    const { articleId } = req.params;
-    const article = articlesInfo.find(a => a.name === articleId);
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+
+    const article = await db.collection('articles').findOne({ name });
+
+    res.json(article);
+})
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
     if (article) {
     article.upvotes +=1;
-    res.send(`The ${articleId} article now has ${article.upvotes} upvotes!!`);
+    res.send(`The ${name} article now has ${article.upvotes} upvotes!!`);
     }else {
         res.send(`That article does not exist`);
     }
 });
 
-app.post('/api/articles/:articleId/comments', (req, res) => {
+app.post('/api/articles/:name/comments', (req, res) => {
     const { postedBy, text } = req.body;
-    const { articleId } = req.params;
+    const { name } = req.params;
 
-    const article = articlesInfo.find(a => a.name === articleId);
+    const article = articlesInfo.find(a => a.name === name);
     if (article) {
     article.comments.push({ postedBy, text });
     res.send(article.comments);
