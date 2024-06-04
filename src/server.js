c7f9044d0f5db0dbe3a6420f7bf4f345ea116390
Marianne Anderson <mganderson13@gmail.com>
2024-06-04 10:02:16 -0500

    refactored db connection to be reusable script

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
 import express from "express";
-import { MongoClient } from 'mongodb';
+import { db, connectToDb } from './db.js';
 
 const app = express();
 app.use(express.json());
 
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
-
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
 
     const article = await db.collection('articles').findOne({ name });
 
@@ -24,13 +19,10 @@ app.get('/api/articles/:name', async (req, res) => {
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
     await db.collection('articles').updateOne({ name }, {
         $inc: { upvotes: 1 },
     });
+
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
@@ -44,13 +36,10 @@ app.post('/api/articles/:name/comments', async (req, res) => {
     const { postedBy, text } = req.body;
     const { name } = req.params;
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
     await db.collection('articles').updateOne({ name }, {
         $push: { comments: { postedBy, text } },
     });
+    
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
@@ -67,7 +56,10 @@ app.post('/api/articles/:name/comments', async (req, res) => {
 //     res.send(`Hello ${name}!!`); can than use value of name param in response
 // })
 
+connectToDb(() => {
+    console.log('Successfully connected to database')
 app.listen(8000, () => {
     console.log("Server is listening on port 8000");
+});
 });
 
